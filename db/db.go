be312b4baabc9bbce82ebb53f5357422c6e5993f
Topseen-a <abodunrintopseen@gmail.com>
2026-05-03 14:04:58 +0100

    ginFlux

--- db/db.go
+++ db/db.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"goGin/config"
+	"goGin/models"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect(cfg config.Config) {
+	var err error
+	DB, err = gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+
+	if err := DB.AutoMigrate(&models.Task{}); err != nil {
+		log.Fatal("Migration failed", err)
+	}
+
+	log.Println("Database connected and migrated")
+}
