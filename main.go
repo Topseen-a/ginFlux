3a10b3f9b502ff7643d2724dde7957009fad642e
Topseen-a <abodunrintopseen@gmail.com>
2026-05-13 14:42:29 +0100

    ginFlux

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"goGin/config"
+	"goGin/db"
 	"goGin/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println(".env file not found")
+	}
+
+	cfg := config.Load()
+	db.Connect(cfg)
+
 	r := gin.Default()
 	routes.SetupRoutes(r)
-	r.Run(":8080")
+
+	r.Run(":" + cfg.Port)
 }
