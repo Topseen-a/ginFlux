fa976037175f83353e97f936ae635da86bc02fd7
Topseen-a <abodunrintopseen@gmail.com>
2026-05-14 12:00:44 +0100

    ginFlux

--- db/db.go
+++ db/db.go
@@ -13,12 +13,14 @@ var DB *gorm.DB
 
 func Connect(cfg config.Config) {
 	var err error
+
 	DB, err = gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Failed to connect to database", err)
 	}
-
-	if err := DB.AutoMigrate(&models.Task{}); err != nil {
+	
+	if err := DB.AutoMigrate(&models.User{}, &models.Task{}); err != nil {
 		log.Fatal("Migration failed", err)
 	}
 
